a5bf1d2bc6d7e485e8a939567bd482cd035b04da
shreyas <shreyas.waykar123@gmail.com>
2022-09-08 11:21:37 +0530

    added resume

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,17 +2,31 @@ import React from "react";
 
 const Projects = () => {
   return (
-    <div className="grid md:grid-cols-2 bg-black text-white p-6 justify-around gap-[50px] w-[80%] mx-auto rounded-md translate-y-[50px] ">
-      <div className="text-xl md:text-2xl font-semibold md:font-bold mx-auto">
-        To see my project repos visit my Github page:
+    <>
+      <div className="grid md:grid-cols-2 bg-black text-white p-6 justify-around gap-[50px] w-[80%] my-[50px] mx-auto rounded-md translate-y-[50px] ">
+        <div className="text-xl md:text-2xl font-semibold md:font-bold mx-auto">
+          To see my project repos visit my Github page:
+        </div>
+        <div className="mx-auto text-xl">
+          {" "}
+          <a href="https://github.com/username-copied">
+            username-copied's Github
+          </a>{" "}
+        </div>
       </div>
-      <div className="mx-auto text-xl">
-        {" "}
-        <a href="https://github.com/username-copied">
-          username-copied's Github
-        </a>{" "}
+
+      <div className="grid md:grid-cols-2 bg-white text-black p-6 justify-around gap-[50px] w-[80%] mx-auto rounded-md translate-y-[50px] ">
+        <p className="text-xl md:text-2xl font-semibold md:font-bold mx-auto">
+          Resume:
+        </p>
+        <a
+          className="mx-auto text-xl"
+          href="https://drive.google.com/file/d/1xGjgM9eMYp-udvVDgHN0Y1ktfiZOa-Ws/view?usp=sharing"
+        >
+          Resume Link
+        </a>
       </div>
-    </div>
+    </>
   );
 };
 
